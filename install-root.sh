ee61ee56f1898cadf8a8e14e401d881ffd3d95ce
Phillip Jackson <philwinkle@gmail.com>
2013-07-01 10:39:57 -0400

    Update channel-discover for MageTool site

--- install-root.sh
+++ install-root.sh
@@ -2,5 +2,6 @@
 pear upgrade pear
 pear channel-discover pear.zfcampus.org
 pear install zfcampus/zf
+pear channel-discover pear.magetool.co.uk
 pear install magetool/magetool-beta
 echo 'basicloader.classes.1 = "MageTool_Tool_Manifest"' | tee ~/.zf.ini
